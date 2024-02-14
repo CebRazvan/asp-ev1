725a0608ed33d83577ea566bcc7dbb26e7e7ec98
Razvan-Ionut Cebotari <127121767+CebRazvan@users.noreply.github.com>
2024-02-14 10:59:34 +0200

    Add files via upload

---
+++ CebotariRazvan-Ionut/Controllers/AdmitereController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CebotariRazvan_Ionut.Controllers
+{
+    public class AdmitereController : Controller
+    {
+        // GET: AdmitereController
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: AdmitereController/Create
+        [HttpPost]
+        public IActionResult Index(double limInstruire, double limStraina, double matematica, double informatica, double mediaExamAbsolvire)
+        {
+            if (limInstruire <= 0 || limStraina <= 0 || matematica <= 0 || informatica <= 0 || mediaExamAbsolvire <= 0)
+            {
+                return View("Index", "Introduceti toate datele!");
+            }
+
+            double media = (limInstruire + limStraina + matematica + informatica + mediaExamAbsolvire) / 5;
+            return View("Index" ,$"Media dumneavoastra de concurs este: {media:F2}");
+        }
+    }
+}
